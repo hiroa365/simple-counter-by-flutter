6b4a2d467a4bc5dba8f3ccc9971464ed34e32e67
Your Name <you@example.com>
2023-02-21 09:36:53 +0900

    ユースケース処理作成

--- lib/ui/socind_page/second_page_view_model.dart
+++ lib/ui/socind_page/second_page_view_model.dart
@@ -3,15 +3,48 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simplecounterbyflutter/device/device_controller.dart';
 
 final secondPageViewModelProvider =
-    ChangeNotifierProvider.autoDispose((ref) => SecondPageViewModel());
+ChangeNotifierProvider.autoDispose((ref) => SecondPageViewModel());
 
 class SecondPageViewModel extends ChangeNotifier {
   SecondPageViewModel();
 
-  final _deviceController = DeviceController();
+  final _usecase = ReadMagStripeUseCaseImpl();
 
   void onClickReadMSButton() {
     debugPrint('SecondPageViewModel: onClickReadMSButton');
-    _deviceController.readMagStripe();
+    _usecase.call().then((result) {
+      //戻り値の分岐処理
+    });
+  }
+}
+
+///インターフェース定義
+abstract class ReadMagStripeUseCase {
+  Future<ReadMagStripeResult> call();
+}
+
+///戻り値の定義は、返却データを追加可能なkotlinのsealed class型を使う
+abstract class ReadMagStripeResult {}
+
+@immutable
+class ReadMagStripeSuccess implements ReadMagStripeResult {}
+
+@immutable
+class ReadMagStripeFooError implements ReadMagStripeResult {}
+
+class ReadMagStripeUseCaseImpl implements ReadMagStripeUseCase {
+  final _deviceController = DeviceController();
+
+  @override
+  Future<ReadMagStripeResult> call() {
+    //ここでは、時系列の処理を直列で記載する
+    //repositoryやdeviceのAPIを呼び出す
+    _deviceController.readMagStripe().then((result){
+      //戻り値の分岐処理
+    });
+
+    //Futureって直列で書ける？
+
+    return Future.value(ReadMagStripeSuccess());
   }
 }
